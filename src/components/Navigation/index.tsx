ec27840a51629495610ba340cae4d035afa63b0d
Nathanael Cruz <nathanael.cruz.fd@gmail.com>
2020-08-26 19:55:46 -0300

    Adjust API accessibility

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -39,7 +39,8 @@ const MenuNavigation: React.FunctionComponent<MenuNavigationPropsItems> = (props
                     })}
                 </ul>
                 <section className="menu-callaction">
-                    <Button
+                    <Button 
+                        aria-label="btn-login"
                         title={props.titleButton} 
                         classNameButton="btn-login"
                     />
@@ -47,7 +48,7 @@ const MenuNavigation: React.FunctionComponent<MenuNavigationPropsItems> = (props
             </section>
             <div className="btn-default btn-hamburguer"
             onClick={setClassMenu}
-            role="button" >
+            role="button" aria-label="btn-hamburguer" >
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="16" role="Ícone mobile">
                     <g fill="#242942" fill-rule="evenodd">
                         <path d="M0 0h24v2H0zM0 7h24v2H0zM0 14h24v2H0z"/>
